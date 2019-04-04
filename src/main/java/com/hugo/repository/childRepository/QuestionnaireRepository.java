7182178659605ce9efbb88845a6eec8d811e0903
luxiaosong <xiaosong.lu@haorui-tech.com>
2019-04-04 15:01:30 +0800

    新建questionnaire相关类

---
+++ src/main/java/com/hugo/repository/childRepository/QuestionnaireRepository.java
@@ -0,0 +1,10 @@
+package com.hugo.repository.childRepository;
+
+import com.hugo.entity.Questionnaire;
+import com.hugo.repository.DomainRepository;
+
+/**
+ * Created by lxs on 2019/4/4.
+ */
+public interface QuestionnaireRepository extends DomainRepository<Questionnaire,Integer> {
+}
