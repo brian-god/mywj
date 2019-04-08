c70a08f27c008317bd683d961dd18ca9ba325fb2
codeChengeWord <tangxkwork@163.com>
2019-04-08 19:28:48 +0800

    改变页面结构校验完成

--- src/main/java/com/hugo/repository/childRepository/QuestionnaireRepository.java
+++ src/main/java/com/hugo/repository/childRepository/QuestionnaireRepository.java
@@ -3,8 +3,16 @@ package com.hugo.repository.childRepository;
 import com.hugo.entity.Questionnaire;
 import com.hugo.repository.DomainRepository;
 
+import java.util.List;
+
 /**
  * Created by lxs on 2019/4/4.
  */
 public interface QuestionnaireRepository extends DomainRepository<Questionnaire,Integer> {
+    /**
+     *
+     * @param userid
+     * @return
+     */
+    List<Questionnaire> getQuestionnaireByiUserId(Integer userid);
 }
