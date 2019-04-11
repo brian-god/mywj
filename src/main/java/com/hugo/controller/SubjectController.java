9555857606a7d9f8df8fadec968e4afbcbefea91
luxiaosong <xiaosong.lu@haorui-tech.com>
2019-04-11 17:21:23 +0800

    调整问卷

---
+++ src/main/java/com/hugo/controller/SubjectController.java
@@ -0,0 +1,29 @@
+package com.hugo.controller;
+
+import com.hugo.entity.Subject;
+import com.hugo.services.SubjectService;
+import com.hugo.utils.QAResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by lxs on 2019/4/11.
+ */
+@Controller
+public class SubjectController {
+
+    @Autowired
+    private SubjectService subjectService;
+
+    @PostMapping("/addSubject")
+    @ResponseBody
+    public QAResult addSubject(Subject subject){
+        if (StringUtils.isEmpty(subject.getSubject())){
+            return QAResult.build(400,"题目为空！！！");
+        }
+        return subjectService.addSubject(subject);
+    }
+}
---
+++ src/main/java/com/hugo/services/SubjectService.java
@@ -0,0 +1,13 @@
+package com.hugo.services;
+
+import com.hugo.entity.Subject;
+import com.hugo.utils.QAResult;
+
+/**
+ * Created by lxs on 2019/4/11.
+ */
+public interface SubjectService {
+
+    QAResult addSubject(Subject subject);
+
+}
